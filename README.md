200de7c6025348e0e7dcddc42180ae138d28daf0
Sherry Li <sherryli@Sherrys-MacBook-Pro.local>
2017-12-15 14:08:54 +1100

    project1 half done

---
+++ README.md
@@ -0,0 +1,51 @@
+Synopsis
+
+tic tac toe is a  game for two players, with two different, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
+
+Code Example
+
+function gameEnd() {
+    alert("Draw! Want to another try? Please refresh the page first.");
+}
+
+function checkMatch() {
+
+for () {
+		if(numOfClicks > 5 && numOfClicks < 9) {
+
+}else if(numOfClicks === 9) {
+			if(winningCombinations[i][0].style.backgroundColor !== 'blue' || winningCombinations[i][1].style.backgroundColor !== 'blue' || winningCombinations[i][2].style.backgroundColor !== 'blue') {
+				console.log('draw');
+			} else if(winningCombinations[i][0].style.backgroundColor !== 'red' || winningCombinations[i][1].style.backgroundColor !== 'red' || winningCombinations[i][2].style.backgroundColor !== 'red') {
+				console.log('draw');
+				gameEnd();
+
+Motivation
+
+Tic-Tac -Toe (along with a lot of other games) involves looking ahead and trying to figure out what the person playing against you might do next.
+
+Installation
+
+Provide code examples and explanations of how to get the project.
+
+API Reference
+
+Depending on the size of the project, if it is small and simple enough the reference docs can be added to the README. For medium size to larger projects it is important to at least provide a link to where the API reference docs live.
+
+Tests
+
+- in this case, please remember always refresh your page first.
+- click any box of the game board to start the game.
+- once make virtical,horizontal,diagonal row to the same color, which wins, otherwise draw and can start a new game by refresh the page.
+
+
+Contributors
+
+important links to help slove problems
+http://www.color-hex.com/
+https://fonts.google.com/
+https://www.w3schools.com
+
+License
+
+A short snippet describing the license (MIT, Apache, etc.)
--- game.js
+++ game.js
@@ -1,14 +1,15 @@
-var bOne = document.querySelectorAll('.b1'); 
-var bTwo = document.querySelectorAll('.b2'); 
-var bThree = document.querySelectorAll('.b3'); 
-var bFour = document.querySelectorAll('.b4'); 
-var bFive = document.querySelectorAll('.b5'); 
-var bSix = document.querySelectorAll('.b6'); 
-var bSeven = document.querySelectorAll('.b7');
-var bEight = document.querySelectorAll('.b8');
-var bNine = document.querySelectorAll('.b9');
+console.log("Ready for Game!")
+
+var bOne = document.querySelector('.b1'); 
+var bTwo = document.querySelector('.b2'); 
+var bThree = document.querySelector('.b3'); 
+var bFour = document.querySelector('.b4'); 
+var bFive = document.querySelector('.b5'); 
+var bSix = document.querySelector('.b6'); 
+var bSeven = document.querySelector('.b7');
+var bEight = document.querySelector('.b8');
+var bNine = document.querySelector('.b9');
 var allBoxes = document.querySelectorAll('.box');
-var board = "";
 
 var rowOne = [bOne, bTwo, bThree];
 var rowTwo = [bFour, bFive, bSix];
@@ -21,29 +22,138 @@ var crossOne = [bOne, bFive, bNine];
 var crossTwo = [bThree, bFive, bSeven];
 var winningCombinations = [rowOne, rowTwo, rowThree, colOne, colTwo, colThree, crossOne, crossTwo];
 
-var numOfClicks = 0;
-function boardChangeColor(event) {	
-	if(numOfClicks % 2 === 0) {
-		event.target.style.backgroundColor = 'red';
-	} else {
-		event.target.style.backgroundColor = 'blue';
-	};
-	numOfClicks = numOfClicks + 1;
-}
+var numOfClicks = 1;
 
-function checkMatch(event) {
-	if(bOne.backgroundColor === bTwo.backgroundColor === bThree.backgroundColor) {
-		console.log('red wins');
-	} 
-	// }
-	 board = bOne.value
-}
-
-allBoxes.forEach(function(eachBox) {
+function boardChangeColor(event) {
+	var playerName="player 1";
 	
-	eachBox.addEventListener('click', boardChangeColor)
-	eachBox.addEventListener('click',checkMatch)
+	if(numOfClicks % 2 === 0) {
+		event.target.style.backgroundColor = 'red'; 
+		playerName = "player 2";		
+	} else {
+		event.target.style.backgroundColor = 'blue'; 				 
+	};
+	console.log("Step1 works :)");
+	checkMatch();
+	numOfClicks = numOfClicks + 1;		
+}
+
+function bluegameEnd() {
+    alert("player 1 wins!  Want to another try? Please refresh the page first.");
+}
+
+function redgameEnd() {
+    alert("player2 wins!  Want to another try? Please refresh the page first.");
+}
+
+function gameEnd() {
+    alert("Draw! Want to another try? Please refresh the page first.");
+}
+
+function checkMatch() {
+	for (var i = 0; i < winningCombinations.length; i++) {
+		if(numOfClicks > 5 && numOfClicks < 9) {
+			if(winningCombinations[i][0].style.backgroundColor === 'blue' && winningCombinations[i][1].style.backgroundColor === 'blue' && winningCombinations[i][2].style.backgroundColor === 'blue') {
+				console.log('player 1 wins');
+				bluegameEnd();	
+			} else if(winningCombinations[i][0].style.backgroundColor === 'red' && winningCombinations[i][1].style.backgroundColor === 'red' && winningCombinations[i][2].style.backgroundColor === 'red') {
+				console.log('player 2 wins');
+				redgameEnd();
+			} 
+		} else if(numOfClicks === 9) {
+			if(winningCombinations[i][0].style.backgroundColor !== 'blue' || winningCombinations[i][1].style.backgroundColor !== 'blue' || winningCombinations[i][2].style.backgroundColor !== 'blue') {
+				console.log('draw');
+			} else if(winningCombinations[i][0].style.backgroundColor !== 'red' || winningCombinations[i][1].style.backgroundColor !== 'red' || winningCombinations[i][2].style.backgroundColor !== 'red') {
+				console.log('draw');
+				gameEnd();
+			}
+
+		}
+		
+	}  
+}
+
+allBoxes.forEach(function(eachBox) {	
+	eachBox.addEventListener("click", boardChangeColor)
+
 })
+
+// var pOneTotalClicks = 1;
+// var pTwoTotalClicks = 2;
+// var pOneTotalClicks = "";
+// var pTwoTotalClicks = "";
+// var symKnot = document.getElementById("").src = imgknot; 
+// var symCross = document.getElementById("").src = imgCross;
+
+// pTwoTotalClicks = pTwoTotalClicks + 2;
+// pOneTotalClicks = pOneTotalClicks + 1;
+
+	//}
+//}
+	// if(numOfClicks > 5 && numOfClicks < 9) {
+	// 	if(bOne.style.backgroundColor === 'blue' && bTwo.style.backgroundColor === 'blue' && bThree.style.backgroundColor === 'blue' || 
+	// 	bFour.style.backgroundColor === 'blue' && bFive.style.backgroundColor === 'blue' && bSix.style.backgroundColor === 'blue' ||
+	// 	bSeven.style.backgroundColor === 'blue' && bEight.style.backgroundColor === 'blue' && bNine.style.backgroundColor === 'blue' ||
+	// 	bOne.style.backgroundColor === 'blue' && bFour.style.backgroundColor === 'blue' && bSeven.style.backgroundColor === 'blue' ||
+	// 	bTwo.style.backgroundColor === 'blue' && bFive.style.backgroundColor === 'blue' && bEight.style.backgroundColor === 'blue' ||
+	// 	bThree.style.backgroundColor === 'blue' && bSix.style.backgroundColor === 'blue' && bNine.style.backgroundColor === 'blue' ||
+	// 	bOne.style.backgroundColor === 'blue' && bFive.style.backgroundColor === 'blue' && bNine.style.backgroundColor === 'blue' ||
+	// 	bThree.style.backgroundColor === 'blue' && bFive.style.backgroundColor === 'blue' && bSeven.style.backgroundColor === 'blue') {
+	// 	console.log('player 1 wins');
+	// 	} else if(bOne.style.backgroundColor === 'red' && bTwo.style.backgroundColor === 'red' && bThree.style.backgroundColor === 'red' || 
+	// 	bFour.style.backgroundColor === 'red' && bFive.style.backgroundColor === 'red' && bSix.style.backgroundColor === 'red' ||
+	// 	bSeven.style.backgroundColor === 'red' && bEight.style.backgroundColor === 'red' && bNine.style.backgroundColor === 'red' ||
+	// 	bOne.style.backgroundColor === 'red' && bFour.style.backgroundColor === 'red' && bSeven.style.backgroundColor === 'red' ||
+	// 	bTwo.style.backgroundColor === 'red' && bFive.style.backgroundColor === 'red' && bEight.style.backgroundColor === 'red' ||
+	// 	bThree.style.backgroundColor === 'red' && bSix.style.backgroundColor === 'red' && bNine.style.backgroundColor === 'red' ||
+	// 	bOne.style.backgroundColor === 'red' && bFive.style.backgroundColor === 'red' && bNine.style.backgroundColor === 'red' ||
+	// 	bThree.style.backgroundColor === 'red' && bFive.style.backgroundColor === 'red' && bSeven.style.backgroundColor === 'red') {
+	// 	console.log('player 2 wins');
+	// 	} else {
+	// 		console.log("Draw!");
+	// 	}
+	// } 
+//}
+	
+	// for (var i = 0; i < winningCombinations.length; i++) {
+
+	// }	
+	// var pOneSum = winningCombinations[0].length * pOneTotalClicks;
+	// var pTwoSum = winningCombinations[0].length * pTwoTotalClicks;
+	// if (pOneSum === 3) {
+	// 	console.log("player 1 wins!");
+	// } else if (pTwoSum === 6) {
+	// 	console.log("player 2 wins!");
+	// } else {
+	// 	console.log("draw");
+	// }
+
+
+
+	//eachBox.addEventListener('click',checkMatch)
+
+// step1: make function for find player 1 and 2 's pattern -- update the infomation
+// step 2 : problem is how to use if statement (and foreach) to check their patterns.
+// calculate the sum for one of winningCombinations to check its result whether === player1's pattern or player2's pattern when run the ( whick? )function or loop the all boxes. 
+
+// step 3:   find out the winner by running the function which can 
+// step 4:  //document.getElementById("i2").src =img1;
+
+
+// 	pOnePattern = event.target.style.backgroundColor;
+// 	pTwoPattern = event.target.style.backgroundColor;
+// }
+// var createBoard = function() {
+//     for (var i = 0; i < allBoxes.length; i++) {
+//         var cardElement = document.createElement('img');
+//         cardElement.setAttribute('src', 'images/back.png');
+//         cardElement.setAttribute('data-id', i);
+//         cardElement.addEventListener('click', flipCard);
+//         document.getElementById('game-board').appendChild(cardElement); 
+//   }   
+// };
+ 
+//createBoard();
 
 // add 2 players by 
 // first, take turns to click ...??need to figue out how to make two different color work by taking turns to click.
@@ -76,16 +186,7 @@ allBoxes.forEach(function(eachBox) {
 //     }	
 // })
 
-// var columns = {
-// colOne: [bOne, bFour, bSeven],
-// colTwo: [bTwo, bFive, bEight],
-// colThree: [bThree, bSix, bNine]
-// }
 
-// var crosses = {
-// crossOne: [bOne, bFive, bNine],
-// crossTwo: [bThree, bFive, bSeven]
-// }
 // var crossesInBoard = Object.keys(crosses);
 // console.log(crossesInBoard);
 
